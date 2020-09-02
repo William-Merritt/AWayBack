8792b9cf39b75123fbb789eca00ff57a388f6fb4
goodingc5066 <47424280+goodingc5066@users.noreply.github.com>
2020-09-01 20:17:03 -0400

    added mobs and living create classes to prgram
    added mobs and living creature classes to the program

--- AWay Back/GameWorld/Mobs.cs
+++ AWay Back/GameWorld/Mobs.cs
@@ -4,7 +4,34 @@ using System.Text;
 
 namespace GameWorld
 {
-    public class Mobs
+    public class Mobs : LivingCreatures
     {
+        //Fields
+
+        private string _description;
+        private string _attributes;
+
+
+
+        //constructors
+
+        //custom
+        public Mobs(int health, string name, string description, string attributes) :
+            base(health, name)
+        {
+
+            Description = description;
+
+            Attributes = attributes;
+
+        }
+        //fullproperties
+
+
+        public string Description { get; set; }
+
+        public string Attributes { get; set; }
+
+
     }
 }
