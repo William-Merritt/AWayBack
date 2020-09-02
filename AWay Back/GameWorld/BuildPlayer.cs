1b21c3eb52eaa075536834b0ad8dd4d9b92904db
Quentin-Merritt <38696938+Quentin-Merritt@users.noreply.github.com>
2020-09-01 20:15:46 -0400

    Adding Player Class

--- AWay Back/GameWorld/BuildPlayer.cs
+++ AWay Back/GameWorld/BuildPlayer.cs
@@ -4,6 +4,45 @@ namespace GameWorld
 {
     public class BuildPlayer
     {
-        
+        public static void BuildAPlayer(Player inputPlayer)
+        {
+
+            Console.WriteLine("Please create a password for your player");
+            Console.Write("---> ");
+            inputPlayer.Password = Console.ReadLine();
+
+            Console.WriteLine("What is the first name of your player?");
+            Console.Write("---> ");
+            inputPlayer.FirstName = Console.ReadLine();
+
+            Console.WriteLine("What is the last name of your player?");
+            Console.Write("---> ");
+            inputPlayer.LastName = Console.ReadLine();
+
+            Console.WriteLine("Please select a class: Swordsman/Lancer/Warrior/Marksman");
+            Console.Write("---> ");
+            inputPlayer.PlayerClass = Console.ReadLine();
+
+            Console.WriteLine("Please select a race: Human/Dwarf/Dracokin/Elf");
+            Console.Write("---> ");
+            inputPlayer.Race = Console.ReadLine();
+
+            if (inputPlayer.Race == "Human")
+            {
+                inputPlayer.Health = 150;
+            }
+            else if (inputPlayer.Race == "Dwarf")
+            {
+                inputPlayer.Health = 220;
+            }
+            else if (inputPlayer.Race == "Dracokin")
+            {
+                inputPlayer.Health = 300;
+            }
+            else
+            {
+                inputPlayer.Health = 190;
+            }
+        }
     }
 }
