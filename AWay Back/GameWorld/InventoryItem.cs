8c307c4a3f4a5bcaf7a76581d2c9286322ecc468
goodingc5066 <47424280+goodingc5066@users.noreply.github.com>
2020-09-01 20:14:08 -0400

    added iems and Iventroy items to program
    added the inventory items and item classes to the program

--- AWay Back/GameWorld/InventoryItem.cs
+++ AWay Back/GameWorld/InventoryItem.cs
@@ -4,7 +4,32 @@ using System.Text;
 
 namespace GameWorld
 {
-    public class InventoryItem
+    // class which weapon,item,treasure and potions inherit from
+    public class Inventory_Item
     {
+
+
+        public Inventory_Item()
+        {
+            itemName = " ";
+            itemDescription = " ";
+            Price = 0;
+        }
+
+        public Inventory_Item(string itemname, string itemdescription, int price)
+        {
+            itemName = itemname;
+            itemDescription = itemdescription;
+            Price = price;
+
+        }
+
+        public int Price { get; set; }
+        public string itemName { get; set; }
+        public string itemDescription { get; set; }
+
+
+
+
     }
 }
--- AWay Back/GameWorld/Items.cs
+++ AWay Back/GameWorld/Items.cs
@@ -4,7 +4,21 @@ using System.Text;
 
 namespace GameWorld
 {
-    public class Items
+    public class Items : Inventory_Item
     {
+
+
+
+        public Items(string itemname, string itemdescription, string size, string quantity, int price)
+            : base(itemname, itemdescription, price)
+        {
+            Size = size;
+
+            Quantity = quantity;
+        }
+
+        public string Quantity { get; set; }
+
+        public string Size { get; set; }
     }
 }
