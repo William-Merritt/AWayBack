6bee58f1af4939a2f0da7fba7a284ae2657528ac
goodingc5066 <47424280+goodingc5066@users.noreply.github.com>
2020-09-01 20:07:48 -0400

    added rooms to the main program
    added class of rooms to program

--- AWay Back/ConsoleUI/Program.cs
+++ AWay Back/ConsoleUI/Program.cs
@@ -10,6 +10,11 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            Playerlibrary.Rooms[] Rooms = { new Playerlibrary.Rooms("PlanetSurface", "", ""),
+                                              new Playerlibrary.Rooms("CaveEntrance", "" , ""),
+                                             new Playerlibrary.Rooms("StarCraftBack", "" , ""),
+                                             new Playerlibrary.Rooms("StarCraftMiddle", "" , ""),
+                                             new Playerlibrary.Rooms("StarCraftFront", "" , "") };// ARRAY FOR ROOMS
         }
     }
 }
--- AWay Back/GameWorld/BuildPlayer.cs
+++ AWay Back/GameWorld/BuildPlayer.cs
@@ -4,5 +4,6 @@ namespace GameWorld
 {
     public class BuildPlayer
     {
+        
     }
 }
