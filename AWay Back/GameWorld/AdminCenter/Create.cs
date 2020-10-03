ae2c3ea5bfbd4dfb18e5e9a0fd84fddc60321d9d
Quentin-Merritt <38696938+Quentin-Merritt@users.noreply.github.com>
2020-10-03 19:19:06 -0400

    Created our create class
    Created a new folder for admin commands
    Created a class name Create that will add new rooms, mobs, items, etc... To our csv files.

---
+++ AWay Back/GameWorld/AdminCenter/Create.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameWorld.AdminCenter
+{
+    public class Create
+    {
+        public static void PromptForCreation() 
+        {
+            string inputString;                         //Will hold all user input
+
+            Console.Write("What are you making?");
+            Console.WriteLine("Mob, Room, Weapon, Treasure, Item, or Potions:  ");
+            inputString = Console.ReadLine();
+
+            switch (inputString.ToLower()) 
+            {
+                case "mob":
+                    break;
+
+                case "room":
+                    break;
+
+                case "weapon":
+                    break;
+
+                case "treasure":
+                    break;
+
+                case "item":
+                    break;
+
+                case "potions":
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        public static void NewMob() 
+        {
+            string inputString;                     //To hold all of the user input
+            int id;
+            string name;
+            string mobInfo;
+            string mobDmgType;
+            string mobWeakness;
+            int health;
+            string race;
+            bool done = false;
+
+            while (done == false) 
+            {
+                Console.WriteLine("What is the ID for the mob?");
+                Console.Write("1 - 100");
+                inputString = Console.ReadLine();
+
+                if (inputString != null) 
+                {
+                    id = Convert.ToInt32(inputString);
+                    if (id > 1 && id <= 100) 
+                    {
+                        foreach (Mobs mob in IDA.Mob.ToList())
+                        {
+                            if (mob.ID == id) 
+                            {
+                                Console.WriteLine("Error. A mob already contains that ID.");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
