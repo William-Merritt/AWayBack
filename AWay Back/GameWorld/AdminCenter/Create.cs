3f0ec13ed92d361cd5d405cc84163312d9ad6968
Quentin-Merritt <38696938+Quentin-Merritt@users.noreply.github.com>
2020-10-04 20:22:29 -0400

    Changes to the create class.
    changes to the create class

--- AWay Back/GameWorld/AdminCenter/Create.cs
+++ AWay Back/GameWorld/AdminCenter/Create.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
+using System.Linq;
 
 namespace GameWorld.AdminCenter
 {
@@ -62,13 +64,50 @@ namespace GameWorld.AdminCenter
                     id = Convert.ToInt32(inputString);
                     if (id > 1 && id <= 100) 
                     {
-                        //foreach (Mobs mob in IDA.Mob.ToList())
-                        //{
-                        //    if (mob.ID == id) 
-                        //    {
-                        //        Console.WriteLine("Error. A mob already contains that ID.");
-                        //    }
-                        //}
+                        foreach (Mobs mob in IDA.Mob.ToList())
+                        {
+                            if (mob.ID == id)
+                            {
+                                Console.WriteLine("Error. A mob already contains that ID.");
+                            }
+                        }
+
+                        Console.WriteLine("What is the Mob ID: ");
+                        id = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("What is the Mob Name: ");
+                        name = Console.ReadLine();
+
+                        Console.WriteLine("What is the Mob Info: ");
+                        mobInfo = Console.ReadLine();
+
+                        Console.WriteLine("What is the Mob Damage Type: ");
+                        mobDmgType = Console.ReadLine();
+
+                        Console.WriteLine("What is the Mob's Weakness: ");
+                        mobWeakness = Console.ReadLine();
+
+                        Console.WriteLine("Amount of HP: ");
+                        health = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("What is the race of this Mob: ");
+                        race = Console.ReadLine();
+
+                        IDA.Mob.Add(new Mobs(id, name, mobInfo, mobDmgType, mobWeakness, health, race));
+
+                        if (File.Exists(@"../../../GameWorld/TextFiles/Mobs.txt")) 
+                        {
+                            File.AppendAllText(@"../../../GameWorld/TextFiles/Mobs.txt", "" + Environment.NewLine);
+                            File.AppendAllText(@"../../../GameWorld/TextFiles/Mobs.txt", id.ToString() + "," + Environment.NewLine);
+                            File.AppendAllText(@"../../../GameWorld/TextFiles/Mobs.txt", name + "," + Environment.NewLine);
+                            File.AppendAllText(@"../../../GameWorld/TextFiles/Mobs.txt", mobInfo + "," + Environment.NewLine);
+                            File.AppendAllText(@"../../../GameWorld/TextFiles/Mobs.txt", mobDmgType + "," + Environment.NewLine);
+                            File.AppendAllText(@"../../../GameWorld/TextFiles/Mobs.txt", mobWeakness + "," + Environment.NewLine);
+                            File.AppendAllText(@"../../../GameWorld/TextFiles/Mobs.txt", health.ToString() + "," + Environment.NewLine);
+                            File.AppendAllText(@"../../../GameWorld/TextFiles/Mobs.txt", race);
+
+                        }
+                        done = true;
                     }
                 }
             }
