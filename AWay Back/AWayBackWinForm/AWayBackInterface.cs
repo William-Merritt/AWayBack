774e07d45360122b1b4f68940a05f9a63d5cde08
Quentin-Merritt <38696938+Quentin-Merritt@users.noreply.github.com>
2020-10-28 14:13:46 -0400

    new changes

--- AWay Back/AWayBackWinForm/AWayBackInterface.cs
+++ AWay Back/AWayBackWinForm/AWayBackInterface.cs
@@ -7,18 +7,92 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GameWorld;
 
 namespace AWayBackWinForm
 {
     public partial class AWayBackInterface : Form
     {
+        private Player _player;
         public AWayBackInterface()
         {
             InitializeComponent();
+            Builder.Build();
+            Movement.MoveToStart();
+
         }
 
-        private void richTextBox2_TextChanged(object sender, EventArgs e)
+
+        private void moveNorthButton_Click(object sender, EventArgs e)
         {
+            Movement.PlayerMovement("north");
+            roomDescRichTextBox.Text = Player.CurrentRoom.Description;
+            if (Player.CurrentRoom.ExitNorth != -1)
+            {
+                Movement.MovePlayer(IDA.Room[Player.CurrentRoom.ExitNorth]);
+                DisplayCurrentRoom.CurrentRoom();
+                Console.ReadLine();
+            }
+            else
+            {
+                Movement.moveHere = false;
+            }
+
+        }
+
+        private void moveWestButton_Click(object sender, EventArgs e)
+        {
+            Movement.PlayerMovement("west");
+            roomDescRichTextBox.Text = Player.CurrentRoom.Description;
+            if (Player.CurrentRoom.ExitWest != -1)
+            {
+                Movement.MovePlayer(IDA.Room[Player.CurrentRoom.ExitWest]);
+                DisplayCurrentRoom.CurrentRoom();
+                Console.ReadLine();
+            }
+            else
+            {
+                Movement.moveHere = false;
+            }
+
+        }
+
+        private void moveEastButton_Click(object sender, EventArgs e) 
+        {
+            Movement.PlayerMovement("east");
+            roomDescRichTextBox.Text = Player.CurrentRoom.Description;
+
+            if (Player.CurrentRoom.ExitEast != -1)
+            {
+                Movement.MovePlayer(IDA.Room[Player.CurrentRoom.ExitEast]);
+                DisplayCurrentRoom.CurrentRoom();
+                Console.ReadLine();
+            }
+            else
+            {
+                Movement.moveHere = false;
+            }
+        }
+
+        private void moveSouthButton_Click(object sender, EventArgs e)
+        {
+            Movement.PlayerMovement("south");
+            roomDescRichTextBox.Text = Player.CurrentRoom.Description;
+            if (Player.CurrentRoom.ExitSouth != -1)
+            {
+                Movement.MovePlayer(IDA.Room[Player.CurrentRoom.ExitSouth]);
+                DisplayCurrentRoom.CurrentRoom();
+                Console.ReadLine();
+            }
+            else
+            {
+                Movement.moveHere = false;
+            }
+        }
+
+        private void combatButton_Click(object sender, EventArgs e)
+        {
+            Battle.CommenceCombat("attack", _player);
 
         }
     }
