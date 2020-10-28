81413a472069fa87977fac99c00e3c4e2a2a3a61
goodingc5066 <47424280+goodingc5066@users.noreply.github.com>
2020-10-28 13:31:26 -0400

    Created new WPF Form
    Created new WPF form to  Display the game in a WPF Window

---
+++ AWay Back/AWayBackWPFUI/AWayBack.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using GameWorld;
+
+namespace AWayBackWPFUI
+{
+    /// <summary>
+    /// Interaction logic for AWayBack.xaml
+    /// </summary>
+    public partial class AWayBack : Window
+    {
+        public AWayBack()
+        {
+            InitializeComponent();
+            Builder.Build();
+            Movement.MoveToStart();
+        }
+
+        private void westButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void eastButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void southButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void northButton_Click(object sender, RoutedEventArgs e)
+        {
+           
+            Movement.PlayerMovement("north");
+            CurrentRoomTextBlock.Text = Player.CurrentRoom.RoomName;
+        }
+
+        private void scanButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void lookButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void attackButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
